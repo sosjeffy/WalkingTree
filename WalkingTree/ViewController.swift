8bd477d54297e2aace52477e592dab9db42357d0
Jeff Touth <jeffy@Jeffys-MacBook-Pro.local>
2020-06-27 16:41:05 -0700

    Initial Commit

---
+++ WalkingTree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WalkingTree
+//
+//  Created by Jeffy Touth on 6/27/20.
+//  Copyright © 2020 Jeffy Touth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
